ac26e87f93042895e7ec719b54722c0118a8c63a
«Alex» <aser_e@mail.ru>
2022-04-07 00:09:20 +0900

    zadacha3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+
+void fillArray(int[,] arr, int start, int end)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            arr[i, j] = rnd.Next(start, end + 1);
+    }
+}
+
+void printArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write(arr[i, j] + " ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int m = 7, n = 9;
+int[,] array = new int[m, n];
+fillArray(array, 10, 99);
+printArray(array);
