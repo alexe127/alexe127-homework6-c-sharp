f0ae91e28f2253b35782467ccdad5b19fd358e60
«Alex» <aser_e@mail.ru>
2022-04-07 00:16:45 +0900

    zadacha4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,38 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные
+void fillArray(int[,] arr, int start, int end)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            arr[i, j] = rnd.Next(start, end + 1);
+    }
+}
+
+void printArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write(arr[i, j] + " ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void changeArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            if (arr[i, j] % 2 == 0) arr[i, j] = -arr[i, j];
+    }
+
+}
+
+int m = 4, n = 5;
+int[,] array = new int[m, n];
+fillArray(array, 10, 99);
+printArray(array);
+changeArray(array);
+printArray(array);
