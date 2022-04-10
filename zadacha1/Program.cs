5b742dc752d88700ff73f21e19bdc4cb1966dfe5
«Alex» <aser_e@mail.ru>
2022-04-11 01:32:29 +0900

    zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void fillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            arr[i, j] = i + j;
+    }
+}
+
+void printArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write(arr[i, j] + " ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int m = 7, n = 9;
+int[,] array = new int[m, n];
+fillArray(array);
+
+printArray(array);
